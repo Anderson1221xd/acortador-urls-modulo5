fe04367b123899054ed9240484175b0913d4c493
Anderson Castilla <andycastilla.333@gmail.com>
2026-05-20 21:01:55 -0500

    feat: integracion de cloudfront y enrutamiento de short

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,3 +44,62 @@ resource "aws_s3_object" "index_html" {
   source       = "${path.module}/../frontend/index.html"
   content_type = "text/html"
 }
+
+# -----------------------------------------------------------------
+# CONFIGURACIÓN DE CLOUDFRONT PARA EL FRONTEND (MÓDULO 5)
+# -----------------------------------------------------------------
+
+resource "aws_cloudfront_distribution" "frontend_distribution" {
+  origin {
+    # Aquí se ajustó al nombre de tu recurso: aws_s3_bucket.main_bucket
+    domain_name = aws_s3_bucket.main_bucket.bucket_regional_domain_name
+    origin_id   = "S3-Website-Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Website-Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Redirección para simular rutas SPA (/short/{codigo})
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "form_frontend_url" {
   value       = "http://${aws_s3_bucket_website_configuration.web_config.website_endpoint}"
   description = "URL principal para que los usuarios acorten sus links"
 }
+
+output "cloudfront_url" {
+  description = "URL segura de CloudFront para el Frontend del Módulo 5"
+  value       = "https://${aws_cloudfront_distribution.frontend_distribution.domain_name}"
+}
